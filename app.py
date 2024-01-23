2a7698e05697e48f6dfb87dfda0e2720e827a820
Aayush Sinha <aayush_sinha@live.com>
2024-01-23 17:50:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,87 @@
+# Steps field to put my jd
+# uploadpdf
+# pdf to image --> processing --> Google Gemini pro 
+# Prompts Template[Multiple prompts]
+
+import base64
+from dotenv import  load_dotenv
+load_dotenv()
+import streamlit as st 
+import os 
+import io
+import base64
+from PIL import Image
+import pdf2image
+from pdf2image import convert_from_path
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,pdf_content,prompt):
+    model=genai.GenreativeModel('gemini-pro-vision')
+    response = model.generate_content([input,pdf_content[0],prompt])
+    return response.text
+
+
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+        # First page -> content of entire pdf
+        first_page = images[0]
+        # Convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr,format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+        
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data" : base64.b64encode(img_byte_arr).decode() #encode to base64
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No such file uploaded")   
+    
+## Streamlit 
+
+st.set_page_config(page_title="ATS RESUME PRO")
+st.header("Application Tracking System ")
+input_text = st.text_area("Job Description:", key="input")
+uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
+
+if uploaded_file is not None:
+    st.write("File Uploaded Successfully")
+    
+submit1 = st.button("Tell me about your resume")
+#submit2 = st.button("How can I Improvise my skills?")
+submit3 = st.button("Percentage Match")
+
+input_prompt1 = """
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding in the field of data science, big data, full stack, data analyst and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+input_prompt3 = """
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Response is : ")
+        st.write(response)
+    else:
+        st.write("Please Upload a Resume")
+elif submit3:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt3,pdf_content,input_text)
+        st.subheader("The Response is : ")
+        st.write(response)
+    else:
+        st.write("Please Upload a Resume")
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Steps field to put my jd
+# uploadpdf
+# pdf to image --> processing --> Google Gemini pro 
+# Prompts Template[Multiple prompts]
